6d6b7bb1ecd245870cdf9010d8b31e2626107f68
Chance <chancellor.whitaker@eku.edu>
2025-05-13 10:21:25 -0400

    docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: { outDir: "docs" },
   plugins: [react()],
-})
+  base: "",
+});
